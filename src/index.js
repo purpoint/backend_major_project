8b56516008ac45958ac86424c360776348ec1468
Manan Ghodasara <145480583+purpoint@users.noreply.github.com>
2026-07-16 14:38:36 +0530

    custom api response, error handling

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { app } from "./app.js"
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 dotenv.config({
@@ -5,4 +6,12 @@ dotenv.config({
 });
 
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+})
+.catch((err) => {
+    console.log("MongoDB connection failed!:", err);
+});
