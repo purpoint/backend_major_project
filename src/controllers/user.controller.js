f722252c9a3a5258a2cba7cae9b07d58f1958b15
Manan Ghodasara <145480583+purpoint@users.noreply.github.com>
2026-07-22 14:34:08 +0530

    Add updateUserAvatar and updateUserCoverImage  management functions for user profile updates

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -260,6 +260,65 @@ const updateAccountDetails = asyncHandler(async(req,res)=> {
     .json(new ApiResponse(200, user, "Account details updated successfully!!"))
 })
 
+const updateUserAvatar = asyncHandler(async(req, res)=> {
+    const avatarLocalPath = req.file?.path
+
+    if(!avatarLocalPath){
+        throw new ApiError(400, "Avatar file is missing")
+    }
+
+   const avatar = await uploadOnCloudinary(avatarLocalPath)
+
+   if(!avatar.url){
+    throw new ApiError(400, "Error while uploading on avatar")
+   }
+
+   const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+        $set: {
+            avatar: avatar.url
+        }
+    },
+    {new: true}).select("-password")
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, user, "Avatar is updated successfully!")
+    )
+
+})
+
+const updateUserCoverImage = asyncHandler(async(req, res)=> {
+    const coverImageLocalPath = req.file?.path
+
+    if(!coverImageLocalPath){
+        throw new ApiError(400, "Cover Image file is missing")
+    }
+
+   const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+   if(!coverImage.url){
+    throw new ApiError(400, "Error while uploading cover Image")
+   }
+
+   const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+        $set: {
+            coverImage: coverImage.url
+        }
+    },
+    {new: true}).select("-password")
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200 ,user, "Cover Image is updated Successfully!")
+    )
+})
+
 export {
     registerUser,
     loginUser,
@@ -267,5 +326,7 @@ export {
     refreshAccessToken,
     changeCurrentPassword,
     getCurrentUser,
-    updateAccountDetails
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage
 }
