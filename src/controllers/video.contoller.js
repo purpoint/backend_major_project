63f100bddfaecba21dcc6b737123e886bbd3af41
Manan Ghodasara <145480583+purpoint@users.noreply.github.com>
2026-07-29 16:15:01 +0530

    Add togglePublishStatus function to manage video publication state with ownership validation Done with videocontroller functionality.

--- src/controllers/video.contoller.js
+++ src/controllers/video.contoller.js
@@ -246,6 +246,37 @@ const deleteVideo = asyncHandler(async(req, res)=> {
         .json(new ApiResponse(200, {}, "Video deleted successfully"))
 })
 
+const togglePublishStatus = asyncHandler(async(req,res)=> {
+    const {videoId} = req.params
+
+    if(!isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid Video ID")
+    }
+
+    const video = await Video.findById(videoId)
+
+    if (!video) {
+            throw new ApiError(404, "Video not found")
+    }
+
+    if (video.owner.toString() !== req.user._id.toString()) {
+        throw new ApiError(403, "You cannot modify someone else's video")
+    }
+
+    video.isPublished = !video.isPublished
+    await video.save({ validateBeforeSave: false })
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            { isPublished: video.isPublished },
+            video.isPublished ? "Video published" : "Video unpublished"
+        )
+    )
+})
+
 export {
     getAllVideos,
     publishAVideo,
