9278b6186eb17e15f90b2cca08a149acdc0dfdd2
Manan Ghodasara <145480583+purpoint@users.noreply.github.com>
2026-07-31 03:09:33 +0530

    feat: add getVideoComments function to retrieve comments for a specific video with pagination

---
+++ src/controllers/comment.controller.js
@@ -0,0 +1,80 @@
+import mongoose, {isValidObjectId} from "mongoose"
+import { Comment } from "../models/comments.models.js"
+import { Like } from "../models/likes.models.js"
+import { Video } from "../models/video.models.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+
+const getVideoComments = asyncHandler(async(req,res)=> {
+    const {videoId} = req.params
+    const {page = 1, limit = 10} = req.query
+
+    if(!isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid video Id")
+    }
+
+    const video = await Video.findById(videoId)
+
+    if(!video) {
+        throw new ApiError(404, "Video not Found!")
+    }
+
+    const options = {
+        page: Math.max(1, parseInt(page,10) || 1),
+        limit: Math.min(50, Math.max(1, parseInt(limit,10) || 10))
+    }
+
+    const commentAggregate = Comment.aggregate([
+        {
+            $match:{
+                video: new mongoose.Types.ObjectId(videoId)
+            }
+        },
+        {
+            $sort: {
+                createdAt: -1
+            }
+        },
+        {
+            $lookup:{
+                from:"users",
+                localField:"owner",
+                foreignField:"_id",
+                as:"owner",
+                pipeline:[
+                    {$project: {username:1, fullname:1, avatar:1}}
+                ]
+            }
+        },
+        {
+            $lookup: {
+                from:"likes",
+                localField:"_id",
+                foreignField:"comment",
+                as:"likes"
+            }
+        },
+        {
+            $addFields:{
+                owner : {$first: "$owner"},
+                likesCount: {$size: "$likes"},
+                isLiked:{
+                    $in:[req.user._id, "$likes.likedBy"]
+                }
+            }
+        },
+        {
+            $project:{
+                likes:0
+            }
+        }
+    ])
+
+    const comments = await Comment.aggregatePaginate(commentAggregate, options)
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, comments, "Comments fetched successfully"))
+
+})
