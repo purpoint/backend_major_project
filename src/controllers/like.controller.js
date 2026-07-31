fc46c5e9fba1b025f178d0835cc90b659ceede96
Manan Ghodasara <145480583+purpoint@users.noreply.github.com>
2026-07-31 17:30:14 +0530

    Added controller function to toggle Comment likes

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -43,3 +43,44 @@ const toggleVideoLike = asyncHandler(async(req,res)=> {
     .json(new ApiResponse(200, {isLiked:true}, "Video Liked"))
 
 })
+
+const toggleCommentLike = asyncHandler(async (req, res) => {
+    const { commentId } = req.params
+
+    if (!isValidObjectId(commentId)) {
+        throw new ApiError(400, "Invalid comment id")
+    }
+
+    const comment = await Comment.findById(commentId)
+
+    if (!comment) {
+        throw new ApiError(404, "Comment not found")
+    }
+
+    const existingLike = await Like.findOne({
+        comment: commentId,
+        likedBy: req.user._id
+    })
+
+    if (existingLike) {
+        await Like.findByIdAndDelete(existingLike._id)
+
+        return res
+            .status(200)
+            .json(new ApiResponse(200, { isLiked: false }, "Comment unliked"))
+    }
+
+    await Like.create({
+        comment: commentId,
+        likedBy: req.user._id
+    })
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, { isLiked: true }, "Comment liked"))
+})
+
+export {
+    toggleVideoLike,
+    toggleCommentLike,
+}
